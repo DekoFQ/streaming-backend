9771fd495a3d889cd4a50e6f171f7aa3af3ca805
Diego Romero <dieguito12557@gmail.com>
2025-08-01 00:06:30 -0500

    Correccion en las rutas de los clientes

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,5 +1,4 @@
 import {Router} from "express"
-import {register, login, logout, profile, userUpdate} from "../controllers/auth.controller.js"
 import { createClient } from "../controllers/client.controller.js"
 
 const router = Router()
