cd2fc11dfdcef860e6b211a9d7c7232ff6b52241
santi <goez1247@gmail.com>
2025-08-08 11:27:57 -0500

    cambios en controladores para correcta recuperación de los datos

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5501',
 ];
 
-//CORS
+//CORs
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
