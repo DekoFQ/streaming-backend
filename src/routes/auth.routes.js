462fcf26fa1d0a26345998fc3bc63389cde9ed2b
santi <goez1247@gmail.com>
2025-08-08 20:24:01 -0500

    Ajustes controlador y rutas auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,24 @@
-import {Router} from "express"
-import {authRequired} from "../middlewares/validateToken.js"
-import {register, login, logout, profile, userUpdate} from "../controllers/auth.controller.js"
+import { Router } from "express";
+import { authRequired } from "../middlewares/validateToken.js";
+import {
+  register,
+  login,
+  logout,
+  profile,
+  updateProfile,
+  changePassword,
+  deleteAccount
+} from "../controllers/auth.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/register", register)
-router.post("/update/:_id", userUpdate )
-router.post("/login", login)
-router.post("/logout", logout)
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.get("/profile", authRequired, profile)
+router.get("/profile", authRequired, profile);
+router.put("/profile", authRequired, updateProfile);
+router.put("/profile/password", authRequired, changePassword);
+router.delete("/profile", authRequired, deleteAccount);
 
-export default router
-
+export default router;
